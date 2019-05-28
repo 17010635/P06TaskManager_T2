f55d1fc362dafedf39e2b888e207b1d7e42738dc
17010635 <17010635@myrp.edu.sg>
2019-05-28 14:03:38 +0800

    commit

---
+++ app/src/main/java/sg/edu/rp/c347/p06_taskmanager_t2/Task.java
@@ -0,0 +1,31 @@
+package sg.edu.rp.c347.p06_taskmanager_t2;
+
+import java.io.Serializable;
+
+public class Task implements Serializable{
+    private int ID;
+    private String name;
+    private String Description;
+
+    public Task(String name, String Description, int ID) {
+        this.name = name;
+        this.Description = Description;
+        this.ID = ID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+
+
+
+}
