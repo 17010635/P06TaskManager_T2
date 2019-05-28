6dc5e958ad36bbff72e1d1e4a2fd41f8465c0c7a
17010635 <17010635@myrp.edu.sg>
2019-05-28 12:47:53 +0800

    commit

--- app/src/main/java/sg/edu/rp/c347/p06_taskmanager_t2/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/p06_taskmanager_t2/MainActivity.java
@@ -1,13 +1,31 @@
 package sg.edu.rp.c347.p06_taskmanager_t2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAddTask;
+    ListView lvTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddTask = findViewById(R.id.btnAddTask);
+        lvTask = findViewById(R.id.lvTasks);
+
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
