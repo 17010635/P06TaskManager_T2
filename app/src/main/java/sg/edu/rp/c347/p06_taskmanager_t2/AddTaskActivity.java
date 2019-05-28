5c5fb5e1e658ba4abe4cd7f4926a9264c8b255c6
17010635 <17010635@myrp.edu.sg>
2019-05-28 13:16:18 +0800

    commit

--- app/src/main/java/sg/edu/rp/c347/p06_taskmanager_t2/AddTaskActivity.java
+++ app/src/main/java/sg/edu/rp/c347/p06_taskmanager_t2/AddTaskActivity.java
@@ -1,7 +1,9 @@
 package sg.edu.rp.c347.p06_taskmanager_t2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,7 +17,29 @@ public class AddTaskActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
+        etName = findViewById(R.id.etName);
+        etDesc = findViewById(R.id.etDesc);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnCancel = findViewById(R.id.btnCancel);
 
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent();
+                setResult(RESULT_OK, i);
+
+                finish();
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                setResult(RESULT_CANCELED, intent);
+                finish();
+            }
+        });
 
 
 
